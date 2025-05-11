24c9f1cc264afee69739b2ee91f18ee1ab989dd2
Mitch J <mjacovet@kent.edu>
2025-05-10 20:52:02 -0400

    make comments draggable

--- src/client/comments.tsx
+++ src/client/comments.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import {
   atom,
@@ -18,6 +18,7 @@ import {
   TldrawUiMenuGroup,
   DefaultContextMenuContent,
   ToolbarItem,
+  Editor,
 } from "tldraw";
 
 const commentInProgress = atom<null | {
@@ -38,6 +39,63 @@ export class CommentTool extends StateNode {
     }
   }
 }
+
+// i think there's a way to add this to tldraw's page type somehow
+type Comment = {
+  id: string;
+  pageX: number;
+  pageY: number;
+  text: string;
+  author: string;
+};
+
+const CommentStorage = {
+  addComment(editor: Editor, comment: Omit<Comment, "id">) {
+    const currentPage = editor.getCurrentPage();
+    const newCommentId = uniqueId();
+    editor.updatePage({
+      id: currentPage.id,
+      meta: {
+        comments: [
+          ...((currentPage.meta.comments as Comment[]) || []),
+          { ...comment, id: newCommentId },
+        ],
+      },
+    });
+    return newCommentId;
+  },
+  updateComment(editor: Editor, commentId: string, newData: Omit<Comment, "id">) {
+    const currentPage = editor.getCurrentPage();
+    const existingComments = (currentPage.meta.comments || []) as Comment[];
+    const commentToEdit = existingComments.find((c) => c.id === commentId);
+    if (!commentToEdit) {
+      console.error(`Could not find comment with id ${commentId} to edit it`);
+      return;
+    }
+    const updatedComment: Comment = {
+      ...commentToEdit,
+      ...newData,
+    };
+    const newComments = [...existingComments.filter((c) => c.id != commentId), updatedComment];
+    editor.updatePage({
+      id: currentPage.id,
+      meta: {
+        comments: newComments,
+      },
+    });
+  },
+  deleteComment(editor: Editor, commentId: string) {
+    const currentPage = editor.getCurrentPage();
+    editor.updatePage({
+      id: currentPage.id,
+      meta: {
+        comments: ((currentPage.meta.comments as any[]) || []).filter(
+          (comment) => comment.id != commentId
+        ),
+      },
+    });
+  },
+};
 
 const openComments = atom("openComments", [] as string[]);
 
@@ -62,23 +120,12 @@ export const CommentEntry = track(() => {
     if (!pageCoordinates) {
       return;
     }
-    const currentPage = editor.getCurrentPage();
-    const newCommentId = uniqueId();
-    editor.updatePage({
-      id: currentPage.id,
-      meta: {
-        comments: [
-          ...((currentPage.meta.comments as any[]) || []),
-          {
-            id: newCommentId,
-            // matches the horizontally centered display coordinate(s)
-            pageX: pageCoordinates.pageX,
-            pageY: pageCoordinates.pageY,
-            text,
-            author: editor.user.getName(),
-          },
-        ],
-      },
+    const newCommentId = CommentStorage.addComment(editor, {
+      // matches the horizontally centered display coordinate(s)
+      pageX: pageCoordinates.pageX,
+      pageY: pageCoordinates.pageY,
+      text,
+      author: editor.user.getName(),
     });
     openComments.set([...openComments.get(), newCommentId]);
     commentInProgress.set(null);
@@ -166,16 +213,119 @@ export const CommentEntry = track(() => {
   );
 });
 
-// i think there's a way to add this to tldraw's page type somehow
-type Comment = {
-  id: string;
-  pageX: number;
-  pageY: number;
-  text: string;
-  author: string;
-};
-
 const commentTargetedByRightClick = atom("commentTargetedByRightClick", "");
+
+export const CommentComponent = track(
+  ({ comment, isThisOpen }: { comment: Comment; isThisOpen: boolean }) => {
+    const editor = useEditor();
+    const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+    const dragStartPoint = useRef({ x: 0, y: 0 });
+    const isBeingDragged = useRef(false);
+    return (
+      <div
+        key={comment.id}
+        className="custom-comment"
+        style={{
+          position: "absolute",
+          background: "white",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: 2,
+          borderRadius: isThisOpen ? 10 : 20,
+          width: isThisOpen ? undefined : 30,
+          height: isThisOpen ? undefined : 30,
+          // feeble attempt to center on where the user clicked
+          top: comment.pageY + dragOffset.y - (isThisOpen ? 10 : 7),
+          left: comment.pageX + dragOffset.x - 7,
+          fontSize: isThisOpen ? undefined : 25,
+          zIndex: isThisOpen ? 1000000 : 900000,
+          border: "1px solid darkgray",
+          fontFamily: '"tldraw_draw", sans-serif',
+        }}
+        onPointerDown={(e) => {
+          e.stopPropagation();
+          isBeingDragged.current = true;
+          dragStartPoint.current = {
+            x: e.clientX,
+            y: e.clientY,
+          };
+          const handleDrag = (event: TLEventInfo) => {
+            // if this is not a left-click (which is indicated by button === 0)
+            // or a touch event (non-mouse), do not respond to drag
+            if (e.pointerType !== "mouse" || e.button === 0) {
+              if (event.name === "pointer_move") {
+                const zoomLevel = editor.getZoomLevel();
+                // you can only learn it by reading their code, but event.point is
+                // based on .clientX and .clientY, just like how i made
+                // dragStartPoint. we just need to scale the offset to match the
+                // zoom level of the page
+                setDragOffset({
+                  x: (event.point.x - dragStartPoint.current.x) / zoomLevel,
+                  y: (event.point.y - dragStartPoint.current.y) / zoomLevel,
+                });
+              }
+            }
+          };
+          const handleDragEnd = (event: TLEventInfo) => {
+            if (event.name === "pointer_up") {
+              editor.off("event", handleDrag);
+              editor.off("event", handleDragEnd);
+              setDragOffset((current) => {
+                // this if condition is mostly to deal with react strict mode
+                // calling this callback twice. react strict mode, i defy you
+                if (isBeingDragged.current) {
+                  if (Math.abs(current.x) < 5 && Math.abs(current.y) < 5) {
+                    handleClick();
+                  } else {
+                    CommentStorage.updateComment(editor, comment.id, {
+                      ...comment,
+                      pageX: comment.pageX + current.x,
+                      pageY: comment.pageY + current.y,
+                    });
+                  }
+                  isBeingDragged.current = false;
+                }
+                return { x: 0, y: 0 };
+              });
+            }
+          };
+          const handleClick = () => {
+            if (e.pointerType !== "mouse" || e.button === 0) {
+              // handle touch event or left click
+              if (openComments.get().includes(comment.id)) {
+                openComments.set([...openComments.get().filter((id) => id !== comment.id)]);
+              } else {
+                openComments.set([...openComments.get(), comment.id]);
+              }
+            } else if (e.pointerType === "mouse" && e.button == 2) {
+              // handle right click
+              commentTargetedByRightClick.set(comment.id);
+              editor.once("event", (event) => {
+                if (event.name === "pointer_up") {
+                  commentTargetedByRightClick.set("");
+                }
+              });
+            }
+          };
+          editor.on("event", handleDrag);
+          editor.on("event", handleDragEnd);
+        }}
+      >
+        {isThisOpen ? (
+          <div style={{ padding: "2px 4px", width: "max-content", maxWidth: 300 }}>
+            <p style={{ margin: "2px 0", fontWeight: "bold" }}>{comment.author}</p>
+            <p style={{ margin: 0, whiteSpace: "pre-wrap" }}>{comment.text}</p>
+          </div>
+        ) : (
+          comment.author.slice(0, 1).toUpperCase()
+        )}
+      </div>
+    );
+  }
+);
 
 export const CommentDisplay = track(() => {
   const editor = useEditor();
@@ -196,61 +346,8 @@ export const CommentDisplay = track(() => {
   }, []);
 
   return comments.map((comment) => {
-    const screenCoords = { x: comment.pageX, y: comment.pageY };
     const isThisOpen = openComments.get().includes(comment.id);
-    return (
-      <div
-        key={comment.id}
-        className="custom-comment"
-        style={{
-          position: "absolute",
-          background: "white",
-          cursor: "pointer",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          gap: 2,
-          borderRadius: isThisOpen ? 10 : 20,
-          width: isThisOpen ? undefined : 30,
-          height: isThisOpen ? undefined : 30,
-          // feeble attempt to center on where the user clicked
-          top: screenCoords.y - (isThisOpen ? 10 : 7),
-          left: screenCoords.x - 7,
-          fontSize: isThisOpen ? undefined : 25,
-          zIndex: isThisOpen ? 1000000 : 900000,
-          border: "1px solid darkgray",
-          fontFamily: '"tldraw_draw", sans-serif',
-        }}
-        onPointerDown={(e) => {
-          e.stopPropagation();
-          // allegedly, this ignores right clicks
-          if (e.pointerType !== "mouse" || e.button === 0) {
-            if (openComments.get().includes(comment.id)) {
-              openComments.set([...openComments.get().filter((id) => id !== comment.id)]);
-            } else {
-              openComments.set([...openComments.get(), comment.id]);
-            }
-          } else if (e.pointerType === "mouse") {
-            commentTargetedByRightClick.set(comment.id);
-            editor.once("event", (event) => {
-              if (event.name === "pointer_up") {
-                commentTargetedByRightClick.set("");
-              }
-            });
-          }
-        }}
-      >
-        {isThisOpen ? (
-          <div style={{ padding: "2px 4px", width: "max-content", maxWidth: 300 }}>
-            <p style={{ margin: "2px 0", fontWeight: "bold" }}>{comment.author}</p>
-            <p style={{ margin: 0, whiteSpace: "pre-wrap" }}>{comment.text}</p>
-          </div>
-        ) : (
-          comment.author.slice(0, 1).toUpperCase()
-        )}
-      </div>
-    );
+    return <CommentComponent comment={comment} isThisOpen={isThisOpen} key={comment.id} />;
   });
 });
 
@@ -258,15 +355,7 @@ export const ContextMenuWithCommentEdit = track((props: TLUiContextMenuProps) =>
   const commentId = commentTargetedByRightClick.get();
   const editor = useEditor();
   const deleteTargetedComment = () => {
-    const currentPage = editor.getCurrentPage();
-    editor.updatePage({
-      id: currentPage.id,
-      meta: {
-        comments: ((currentPage.meta.comments as any[]) || []).filter(
-          (comment) => comment.id != commentId
-        ),
-      },
-    });
+    CommentStorage.deleteComment(editor, commentId);
     commentTargetedByRightClick.set("");
   };
 
